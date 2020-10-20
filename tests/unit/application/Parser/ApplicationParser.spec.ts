49efe7e5bc10a7dde2531d80c38f37609f3fe9cd
undergroundwires <undergroundwires@users.noreply.github.com>
2020-10-20 14:24:01 +0200

    fix bad merge conflict

--- tests/unit/application/Parser/ApplicationParser.spec.ts
+++ tests/unit/application/Parser/ApplicationParser.spec.ts
@@ -114,12 +114,3 @@ function getProcessEnvironmentStub(): NodeJS.ProcessEnv {
         VUE_APP_HOMEPAGE_URL: 'stub-homepage-url',
     };
 }
-
-function getProcessEnvironmentStub(): NodeJS.ProcessEnv {
-    return {
-        VUE_APP_VERSION: 'stub-version',
-        VUE_APP_NAME: 'stub-name',
-        VUE_APP_REPOSITORY_URL: 'stub-repository-url',
-        VUE_APP_HOMEPAGE_URL: 'stub-homepage-url',
-    };
-}
