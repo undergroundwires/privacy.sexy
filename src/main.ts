7930bef48c4e9a4fe0823673958ed8377f5ee533
undergroundwires <undergroundwires@users.noreply.github.com>
2021-01-21 04:38:02 +0100

    transpile using babel for legacy browser support

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import { ApplicationBootstrapper } from './presentation/Bootstrapping/ApplicationBootstrapper';
-import 'core-js/fn/array/flat-map'; // Here until Vue 3 & CLI v4 https://github.com/vuejs/vue-cli/issues/3834
 
 new ApplicationBootstrapper()
     .bootstrap(Vue);
