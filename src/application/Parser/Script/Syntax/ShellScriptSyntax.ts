236a0f6c8241294fc397194cd1b20bdeccbbb50b
undergroundwires <git@undergroundwires.dev>
2021-10-16 19:49:41 +0200

    Add privacy over security scripts for macOS #83
    It adds scripts to:
      - Disable OS security modules.
      - Clean quarantine data.
      - Disable auto-updates.

--- src/application/Parser/Script/Syntax/ShellScriptSyntax.ts
+++ src/application/Parser/Script/Syntax/ShellScriptSyntax.ts
@@ -2,5 +2,5 @@ import { ILanguageSyntax } from '@/domain/ScriptCode';
 
 export class ShellScriptSyntax implements ILanguageSyntax {
     public readonly commentDelimiters = [ '#' ];
-    public readonly commonCodeParts = [ '(', ')', 'else' ];
+    public readonly commonCodeParts = [ '(', ')', 'else', 'fi' ];
 }
