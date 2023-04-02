9ef0039733c385a4faaaac79ffd9b2a6accccf19
Couleur <82747632+couleurm@users.noreply.github.com>
2023-04-02 10:29:13 +0200

    rename Module to PSModule

--- ci.ps1
+++ ci.ps1
@@ -11,9 +11,9 @@ if (!(Get-Module powershell-yaml)){
 
 $collection = Get-Content .\src\application\collections\windows.yaml | ConvertFrom-Yaml
 
-. ./Convert-PrivacySexyToModule.ps1
+. ./Convert-PrivacySexyToPSModule.ps1
 Copy-Item ./classPrivacySexy.ps1 ./privacy.sexy.ps1
-Convert-PrivacySexyToModule $collection | Add-Content ./privacy.sexy.ps1
+Convert-PrivacySexyToPSModule $collection | Add-Content ./privacy.sexy.ps1
 
 
 
